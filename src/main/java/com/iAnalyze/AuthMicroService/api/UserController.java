89595152441cea381241742b3a80eb2764fa7a07
Jethro Glaudin <jethro_glaudin@my.uri.edu>
2020-05-14 23:43:21 -0400

    refactored loginUser to throw method to throw custom runtime exception

--- src/main/java/com/iAnalyze/AuthMicroService/api/UserController.java
+++ src/main/java/com/iAnalyze/AuthMicroService/api/UserController.java
@@ -1,15 +1,15 @@
 package com.iAnalyze.AuthMicroService.api;
 
+import com.iAnalyze.AuthMicroService.exceptions.CustomRuntimeException;
 import com.iAnalyze.AuthMicroService.jwt.JwtOps;
 import com.iAnalyze.AuthMicroService.models.User;
-import com.iAnalyze.AuthMicroService.models.UsernameAndPasswordAuthenticationRequest;
+import com.iAnalyze.AuthMicroService.models.EmailAndPasswordAuthenticationRequest;
 import com.iAnalyze.AuthMicroService.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -39,30 +39,18 @@ public class UserController {
         userService.registerUser(null, user);
         Map<String, String> map = new HashMap<>();
         map.put("message", "registered successfully");
-        return new ResponseEntity<>(map, HttpStatus.OK);
+        return new ResponseEntity<>(map, HttpStatus.CREATED);
     }
 
     @PostMapping("/login")
-    public ResponseEntity<?> loginUser(@RequestBody UsernameAndPasswordAuthenticationRequest auth) throws Exception {
+    public ResponseEntity<?> loginUser(@RequestBody EmailAndPasswordAuthenticationRequest auth) throws CustomRuntimeException {
         try {
             authenticationManager.authenticate(
                     new UsernamePasswordAuthenticationToken(auth.getEmail(), auth.getPassword())
             );
         } catch (Exception ex) {
-            throw new Exception("inavalid username/password");
+            throw new CustomRuntimeException("Invalid email/password");
         }
         return new ResponseEntity<>(jwtOps.generateToken(auth.getEmail(), auth.getPassword()), HttpStatus.OK);
     }
-
-//    @PostMapping("/login")
-//    public ResponseEntity<?> loginUser(@RequestBody UsernameAndPasswordAuthenticationRequest auth) throws Exception {
-//        User user = userService.validateUser(auth.getEmail(), auth.getPassword());
-//        Authentication newUser = authenticationManager.authenticate(
-//                new UsernamePasswordAuthenticationToken(user.getEmail(), user.getPassword())
-//        );
-//        if (!newUser.isAuthenticated()) {
-//            return new ResponseEntity<>("Login unsuccessful", HttpStatus.UNAUTHORIZED);
-//        }
-//        return new ResponseEntity<>("Login unsuccessful", HttpStatus.OK);
-//    }
 }
