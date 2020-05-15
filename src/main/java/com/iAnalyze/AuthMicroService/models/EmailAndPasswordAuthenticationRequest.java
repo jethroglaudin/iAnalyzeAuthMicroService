0199b4e1fd2f570ce10ea52aedb5d3c20e334387
Jethro Glaudin <jethro_glaudin@my.uri.edu>
2020-05-14 23:42:19 -0400

    renamed file

--- src/main/java/com/iAnalyze/AuthMicroService/models/EmailAndPasswordAuthenticationRequest.java
+++ src/main/java/com/iAnalyze/AuthMicroService/models/EmailAndPasswordAuthenticationRequest.java
@@ -7,7 +7,7 @@ import lombok.NoArgsConstructor;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-public class UsernameAndPasswordAuthenticationRequest {
+public class EmailAndPasswordAuthenticationRequest {
     private String email;
     private String password;
 
