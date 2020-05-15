a378754d524fd428cb5080e75b9ca6e72751b586
Jethro Glaudin <jethro_glaudin@my.uri.edu>
2020-05-14 23:41:39 -0400

    refactored generate token method

--- src/main/java/com/iAnalyze/AuthMicroService/jwt/JwtOps.java
+++ src/main/java/com/iAnalyze/AuthMicroService/jwt/JwtOps.java
@@ -20,29 +20,22 @@ public class JwtOps {
     @Autowired
     UserDao userDao;
 
-    public String generateToken(String email, String password) {
+    public Map<String, String> generateToken(String email, String password) {
         Map<String, Object> claims = new HashMap<>();
         return createToken(email, password);
     }
 
-    private String createToken(String email, String username) {
+    private Map<String, String> createToken(String email, String username) {
         User userDetails = userDao.findByEmail(email);
-//        String token = Jwts.builder()
-//                .signWith(SignatureAlgorithm.HS256, environment.getProperty("token.secret"))
-//                .setIssuedAt(new Date())
-////
-//                .setExpiration(new Date(System.currentTimeMillis() + Long.parseLong(environment.getProperty("token.expiration_time"))))
-//                .claim("id", userDetails.getId())
-//                .claim("username", userDetails.getUsername())
-//                .claim("email", userDetails.getEmail())
-//                .compact();
-
-        return Jwts.builder()
+        String token = environment.getProperty("token.prefix") + Jwts.builder()
                 .claim("id", userDetails.getId())
                 .claim("username", userDetails.getUsername())
                 .claim("email", userDetails.getEmail())
                 .setIssuedAt(new Date(System.currentTimeMillis()))
                 .setExpiration(new Date(System.currentTimeMillis() + Long.parseLong(environment.getProperty("token.expiration_time"))))
                 .signWith(SignatureAlgorithm.HS256, environment.getProperty("token.secret")).compact();
+        Map<String, String> map = new HashMap<>();
+        map.put("token", token);
+        return map;
     }
 }
