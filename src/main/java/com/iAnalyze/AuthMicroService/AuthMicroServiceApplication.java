7ac3f40120624b447f0ff1fb56715dfd5a978846
Jethro Glaudin <jethro_glaudin@my.uri.edu>
2020-05-15 09:36:31 -0400

    Added Enable Euerka Client annotation

--- src/main/java/com/iAnalyze/AuthMicroService/AuthMicroServiceApplication.java
+++ src/main/java/com/iAnalyze/AuthMicroService/AuthMicroServiceApplication.java
@@ -2,10 +2,14 @@ package com.iAnalyze.AuthMicroService;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.loadbalancer.annotation.LoadBalancerClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.authentication.AuthenticationManager;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class AuthMicroServiceApplication {
 
     public static void main(String[] args) {
