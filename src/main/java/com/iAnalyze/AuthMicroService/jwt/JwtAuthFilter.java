7215eb2be088f003c7d2734875120a9b6d610c22
Jethro Glaudin <jethro_glaudin@my.uri.edu>
2020-05-15 09:56:27 -0400

    refactored a few of the jwt method operations for generating and creating tokens and added methods to extract claims and validate a token

--- src/main/java/com/iAnalyze/AuthMicroService/jwt/JwtAuthFilter.java
+++ src/main/java/com/iAnalyze/AuthMicroService/jwt/JwtAuthFilter.java
@@ -1,85 +1,83 @@
-package com.iAnalyze.AuthMicroService.jwt;
-
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.iAnalyze.AuthMicroService.dao.UserDao;
-import com.iAnalyze.AuthMicroService.models.User;
-import com.iAnalyze.AuthMicroService.models.EmailAndPasswordAuthenticationRequest;
-import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.SignatureAlgorithm;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.core.env.Environment;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.AuthenticationException;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-
-import javax.servlet.FilterChain;
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
-import java.util.Date;
-
-public class JwtAuthFilter  extends UsernamePasswordAuthenticationFilter {
-
-//    @Autowired
-//    private final AuthenticationManager authenticationManager;
-    @Autowired
-    private Environment environment;
-    private UserDao userDao;
-
-    public JwtAuthFilter(AuthenticationManager authenticationManager) {
-        super.setAuthenticationManager(authenticationManager);
-    }
-
-    public JwtAuthFilter(UserDao userDao) {
-        this.userDao = userDao;
-    }
-
-    @Override
-    public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response) throws AuthenticationException {
-
-        try {
-            EmailAndPasswordAuthenticationRequest authenticationRequest = new ObjectMapper()
-                    .readValue(request.getInputStream(), EmailAndPasswordAuthenticationRequest.class);
-//            Authentication authentication = new UsernamePasswordAuthenticationToken(
-//                    authenticationRequest.getUsername(),
-//                    authenticationRequest.getPassword()
-//            );
-//            return authenticationManager.authenticate(authentication);
-
-            return getAuthenticationManager().authenticate(
-                    new UsernamePasswordAuthenticationToken(
-                            authenticationRequest.getEmail(),
-                            authenticationRequest.getPassword()
-                    )
-            );
-        } catch(IOException ex) {
-            throw new RuntimeException("Could not attempt authentication");
-        }
-    }
-
-    @Override
-    protected void successfulAuthentication(HttpServletRequest request,
-                                            HttpServletResponse response,
-                                            FilterChain chain,
-                                            Authentication authResult) throws IOException, ServletException {
-        String email = ((User) authResult.getPrincipal()).getEmail();
-//        String password = ((User) authResult.getPrincipal()).getPassword();
-        User userDetails = userDao.findByEmail(email);
-        String token = Jwts.builder()
-                .signWith(SignatureAlgorithm.HS256, environment.getProperty("token.secret"))
-                .setIssuedAt(new Date())
+//package com.iAnalyze.AuthMicroService.jwt;
 //
-                .setExpiration(new Date(System.currentTimeMillis() + Long.parseLong(environment.getProperty("token.expiration_time"))))
-                .claim("id", userDetails.getId())
-                .claim("username", userDetails.getUsername())
-                .claim("email", userDetails.getEmail())
-                .compact();
-
-        System.out.println(token);
-
-        response.addHeader("Authorization", environment.getProperty("token.prefix") + token);
-    }
-}
+//import com.fasterxml.jackson.databind.ObjectMapper;
+//import com.iAnalyze.AuthMicroService.dao.UserDao;
+//import com.iAnalyze.AuthMicroService.models.User;
+//import com.iAnalyze.AuthMicroService.models.UsernameAndPasswordAuthenticationRequest;
+//import io.jsonwebtoken.Jwts;
+//import io.jsonwebtoken.SignatureAlgorithm;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.core.env.Environment;
+//import org.springframework.security.authentication.AuthenticationManager;
+//import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+//import org.springframework.security.core.Authentication;
+//import org.springframework.security.core.AuthenticationException;
+//import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+//import org.springframework.stereotype.Component;
+//
+//import javax.servlet.FilterChain;
+//import javax.servlet.ServletException;
+//import javax.servlet.http.HttpServletRequest;
+//import javax.servlet.http.HttpServletResponse;
+//import java.io.IOException;
+//import java.util.Date;
+//
+//
+//public class JwtAuthFilter  extends UsernamePasswordAuthenticationFilter {
+//
+////    @Autowired
+////    private final AuthenticationManager authenticationManager;
+//    @Autowired
+//    private Environment environment;
+//    private UserDao userDao;
+//
+//    public JwtAuthFilter(AuthenticationManager authenticationManager) {
+//        super.setAuthenticationManager(authenticationManager);
+//    }
+//
+//    public JwtAuthFilter(UserDao userDao) {
+//        this.userDao = userDao;
+//    }
+//
+//    @Override
+//    public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response) throws AuthenticationException {
+//
+//        try {
+//            UsernameAndPasswordAuthenticationRequest authenticationRequest = new ObjectMapper()
+//                    .readValue(request.getInputStream(), UsernameAndPasswordAuthenticationRequest.class);
+////
+//
+//            return getAuthenticationManager().authenticate(
+//                    new UsernamePasswordAuthenticationToken(
+//                            authenticationRequest.getUsername(),
+//                            authenticationRequest.getPassword()
+//                    )
+//            );
+//        } catch(IOException ex) {
+//            throw new RuntimeException("Could not attempt authentication");
+//        }
+//    }
+//
+//    @Override
+//    protected void successfulAuthentication(HttpServletRequest request,
+//                                            HttpServletResponse response,
+//                                            FilterChain chain,
+//                                            Authentication authResult) throws IOException, ServletException {
+//        String email = ((User) authResult.getPrincipal()).getEmail();
+////        String password = ((User) authResult.getPrincipal()).getPassword();
+//        User userDetails = userDao.findByEmail(email);
+//        String token = Jwts.builder()
+//                .signWith(SignatureAlgorithm.HS256, environment.getProperty("token.secret"))
+//                .setIssuedAt(new Date())
+////
+//                .setExpiration(new Date(System.currentTimeMillis() + Long.parseLong(environment.getProperty("token.expiration_time"))))
+//                .claim("id", userDetails.getId())
+//                .claim("username", userDetails.getUsername())
+//                .claim("email", userDetails.getEmail())
+//                .compact();
+//
+//        System.out.println(token);
+//
+//        response.addHeader("Authorization", environment.getProperty("token.prefix") + token);
+//    }
+//}
